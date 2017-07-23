443aa99f4b43a357d2dbe91dad39f6c858f0e62f
JakeLopez <SylviaV@Sylvias-MBP.attlocal.net>
2017-07-22 20:26:16 -0500

    All files (modal not working)

---
+++ app/data/friends.js
@@ -0,0 +1,77 @@
+var friends = [{
+        name: "Sylvia Vaquera",
+        photos: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAerAAAAJGNmOWNmNGFiLTg4NDYtNDA2NS04ZjU0LTM3NmVlOTNhOTA2MA.jpg",
+        scores: [
+            "5",
+            "5",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+
+    {
+        name: "Daniel Alvarez",
+        photos: "https://danielalvarez.me/wp-content/uploads/2014/09/daniel-alvarez-headshot.jpg",
+        scores: [
+            "3",
+            "2",
+            "1",
+            "4",
+            "3",
+            "5",
+            "4",
+            "2",
+            "3",
+            "3"
+
+        ]
+
+    },
+
+    {
+        name: "Sabrina Mendoza",
+        photos: "https://www.facebook.com/photo.php?fbid=10155553673434684&set=a.429182254683.222076.748844683&type=3&theater",
+        scores: [
+            "3",
+            "4",
+            "5",
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5"
+        ]
+    },
+
+    {
+        name: "Miguel Maldonado",
+        photos: "https://www.facebook.com/photo.php?fbid=10153522431128126&set=a.482698768125.270298.675433125&type=3&theater",
+        scores: [
+            "2",
+            "2",
+            "1",
+            "3",
+            "4",
+            "2",
+            "1",
+            "1",
+            "5",
+            "5"
+
+        ]
+
+    }
+
+
+
+];
+
+module.exports = friends;
